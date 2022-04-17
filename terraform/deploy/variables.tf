aec394a4e6f1239452dc0e3c1c128ef4fc4edcfe
Luthiano Trarbach <lmtrarbach@msn.com>
2022-04-17 12:29:04 +0100

    Removing maria db

--- terraform/deploy/variables.tf
+++ terraform/deploy/variables.tf
@@ -1,7 +1,6 @@
 variable "application_name" {
   type = map
   default = {
-     "hostname-api" = "https://lmtrarbach.github.io/hostname-api/",
-     "mariadb"      = "https://charts.bitnami.com/bitnami"
+     "hostname-api" = "https://lmtrarbach.github.io/hostname-api/"
   }
 }
